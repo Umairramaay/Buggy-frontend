797d023c44f759f751cc295f550b1081c12efde4
Umairramaay <59950428+Umairramaay@users.noreply.github.com>
2021-10-10 03:53:10 +0500

    build

---
+++ build/precache-manifest.5b02ba56da8a5561e1b991f103c538d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2ca78da173cbb767f7032b9ca5f939",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91f7bde22fb7c7b1edff",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f9e3c3611aa4e23d2c9c",
+    "url": "/static/js/2.33efdcb9.chunk.js"
+  },
+  {
+    "revision": "3c56d32f1a0e992453f57615992b86b8",
+    "url": "/static/js/2.33efdcb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea9da6dc8f0f36d8841",
+    "url": "/static/js/3.01042de2.chunk.js"
+  },
+  {
+    "revision": "91f7bde22fb7c7b1edff",
+    "url": "/static/js/main.a543e6e3.chunk.js"
+  },
+  {
+    "revision": "5361900ef7ba17c76511",
+    "url": "/static/js/runtime-main.f6ca043e.js"
+  }
+]);
